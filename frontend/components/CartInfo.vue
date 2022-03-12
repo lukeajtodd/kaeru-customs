1084a6cd5e14fd191ec91714fb48ce014bca16d4
Luke Todd <luketodd@zoho.com>
2022-03-12 18:00:26 +0000

    Started icon directory

--- frontend/components/CartInfo.vue
+++ frontend/components/CartInfo.vue
@@ -1,3 +1,6 @@
 <template>
-  <button class="flex items-center">Cart</button>
+  <button class="flex items-center">
+    <IconsCart class="hover:text-brand-primary" />
+    <span class="sr-only">Cart</span>
+  </button>
 </template>
---
+++ frontend/components/icons/Cart.vue
@@ -0,0 +1,33 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    class="iconify iconify--ri"
+    :width="width"
+    :height="height"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 24 24"
+  >
+    <path
+      :fill="fill"
+      d="M4 6.414L.757 3.172l1.415-1.415L5.414 5h15.242a1 1 0 0 1 .958 1.287l-2.4 8a1 1 0 0 1-.958.713H6v2h11v2H5a1 1 0 0 1-1-1V6.414zM6 7v6h11.512l1.8-6H6zm-.5 16a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3zm12 0a1.5 1.5 0 1 1 0-3a1.5 1.5 0 0 1 0 3z"
+    ></path>
+  </svg>
+</template>
+
+<script lang="ts" setup>
+const { width, height, fill } = withDefaults(
+  defineProps<{
+    width: number;
+    height: number;
+    fill: string;
+  }>(),
+  {
+    width: 24,
+    height: 24,
+    fill: "currentColor",
+  }
+);
+</script>
