c78fcd8870ac2b480640c546fce113172bda8349
Luke Todd <luketodd@zoho.com>
2022-03-14 09:22:02 +0000

    Added reel component for reuse

---
+++ frontend/components/product/Reel.vue
@@ -0,0 +1,18 @@
+<template>
+  <Container>
+    <div class="grid grid-cols-12 my-12 md:my-24 gap-4">
+      <div
+        class="col-span-12 md:col-span-6 lg:col-span-3"
+        v-for="product in products"
+      >
+        <CardsProduct :product="product" :key="product.id" />
+      </div>
+    </div>
+  </Container>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  products: any;
+}>();
+</script>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -15,16 +15,7 @@
         />
       </video>
     </div>
-    <Container>
-      <div class="grid grid-cols-12 my-12 md:my-24 gap-4">
-        <div
-          class="col-span-12 md:col-span-6 lg:col-span-3"
-          v-for="product in products.data"
-        >
-          <CardsProduct :product="product" :key="product.id" />
-        </div>
-      </div>
-    </Container>
+    <ProductReel :products="products.data" />
   </NuxtLayout>
 </template>
 
